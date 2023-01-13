4ffa631f652a7da707e85be440dc0c135ce8ea70
YUPENG123 <yupengdongguan@hotmail.com>
2023-01-13 09:22:41 +0000

    modified:   writeFile.js

--- writeFile.js
+++ writeFile.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-fs.writeFile('x/test/writeFile.txt', "HolleWorld", 'utf8', function(err){
+fs.writeFile('./test/writeFile.txt', "HolleWorld", 'utf8', function(err){
 	if(err){
         console.log('文件写入失败！' + err.message)
         return

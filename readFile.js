fd38699aff74f25ec03bdfeb84d834a1035962a1
YUPENG123 <yupengdongguan@hotmail.com>
2023-01-13 09:12:43 +0000

    modified:   readFile.js

--- readFile.js
+++ readFile.js
@@ -1,7 +1,7 @@
 // 1．导入fs模块，来操作文件
 const fs = require("fs")
 // 2.调用fs.readFile()方法读取文件
-fs.readFile('./test/readFiles.txt', 'utf8', function(err, result){
+fs.readFile('./test/readFile.txt', 'utf8', function(err, result){
     if (err){
         console.log('文件读取失败~' + err.message)
         return 

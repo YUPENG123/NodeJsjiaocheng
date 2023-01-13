394d31e265b469c0d0366ce5c065f6ef751e8748
YUPENG123 <yupengdongguan@hotmail.com>
2023-01-13 09:11:37 +0000

    modified:   readFile.js

--- readFile.js
+++ readFile.js
@@ -1,13 +1,10 @@
 // 1．导入fs模块，来操作文件
 const fs = require("fs")
 // 2.调用fs.readFile()方法读取文件
-// 	参数1:读取文件的存放路径
-// 	参数2:读取文件时候采用的编码格式，一般默认指定utf8
-// 	参数3:回调函数，拿到读取失败和成功的结果 err dataStr
-fs.readFile('./test/readFile.txt', 'utf8', function(err, dataStr){
-	// 打印失败的结果
-	console.log(err)
-	console.log("<--------------------------------------------->")
-	// 打印成功的结果
-	console.log(dataStr)
+fs.readFile('./test/readFiles.txt', 'utf8', function(err, result){
+    if (err){
+        console.log('文件读取失败~' + err.message)
+        return 
+    }
+	console.log('文件读取成功，内容是：' + result)
 })
